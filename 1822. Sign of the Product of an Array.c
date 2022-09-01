af03d33dcff7e8aff1d5a18e397bd0a7d5983509
Alessio Vukaj <alessio.vukaj@ryerson.ca>
2022-08-31 20:58:24 -0400

    1822. Sign of the Product of an Array

---
+++ 1822. Sign of the Product of an Array.c
@@ -0,0 +1,18 @@
+int arraySign(int* nums, int numsSize){
+double product=1;
+for(int i=0;i<numsSize;i++){
+    product*=nums[i];
+}
+return signFunc(product);
+}
+
+int signFunc(double product){
+
+    if(product<0){
+        return -1;
+    }
+    else if(product>0){
+        return 1;
+    }
+    return 0;
+}
